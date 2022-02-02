4ae53910f6e69bff44d0054909d475a213d823df
RingtarihTamfu <98171905+RingtarihTamfu@users.noreply.github.com>
2022-02-02 09:48:40 -0600

    More documentation and restructuring

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@ Project: Sumo Bot
 Organization: Texas State University
 
 Description: The purpose of this code to run all the processes of a Raspberry Pi Pico with other peripherals for the sumo bot that we
-             are designing for our Senior Design Project using multicore programming.
+             are designing for our Senior Design Project specifically for the HALLWAY TRAVERSAL
              All statements are documented to show you the flow of the program,
 */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*LIBRARY BLOCK*/
-//All libraries we used in this program.
+/*DESCRIPTION: This block contains all the libraries we used in this program.*/
 #include <Arduino.h>                //This library allows us to use the Arduino format of coding the Raspberry Pi Pico
 #include "pico/multicore.h"         //This library will allow us both cores of the Raspberry Pi Pico
 /*END OF LIBRARY BLOCK*/
@@ -19,6 +19,7 @@ Description: The purpose of this code to run all the processes of a Raspberry Pi
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*INITIALIZE BLOCK*/
+/*DESCRIPTION: This block contains all the pins used in the Pico for every peripheral we used in this project*/
 //Below are the pins used for the new IR sensors for object detection
 #define frontLeftSensor 0
 #define frontMiddleSensor 1
@@ -33,70 +34,25 @@ Description: The purpose of this code to run all the processes of a Raspberry Pi
 #define leftMotorBW 17
 #define rightMotorBW 18
 #define rightMotorFW 19
-
-#define LED 25
-
 #define speedControlL 26
 #define speedControlR 27
 /*END OF INITIALIZE BLOCK*/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*VARIABLE BLOCK*/
-//Below are the variables used in the program.
-//These variables are pretty much RPM values that we want the whwwls to speed up to.
+/*GLOBAL VARIABLE BLOCK*/
+/*DESCRIPTION: This block contains all the variable primarily used for the speed control (RPM) values of the TT motors*/
 int m1Speed = 250;
 int m2Speed = 255;
 int m1Slow = 100;
 int m2Slow = 100;
-/*VARIABLE BLOCK*/
+/*END OF VARIABLE BLOCK*/
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*SETUP BLOCK*/
-/*Description: The block below is the setup block. This block is used to set the modes of each pin that we initialize in the INITIALIZE
-               BLOCK above*/
-void setup() {
-  //Below is the code for the new IR Sensors to set them up
-  pinMode(frontLeftSensor, INPUT_PULLUP);
-  pinMode(frontMiddleSensor, INPUT_PULLUP);
-  pinMode(frontRightSensor, INPUT_PULLUP);
-  ///////////////////////////////////////////////////////////
-  pinMode(IRFrontCenter, INPUT);
-  pinMode(IRBackCenter, INPUT);
-  pinMode(IRFrontLeft, INPUT);
-  pinMode(IRFrontRight, INPUT);
-  pinMode(leftMotorFW, OUTPUT);
-  pinMode(leftMotorBW, OUTPUT);
-  pinMode(rightMotorBW, OUTPUT);
-  pinMode(rightMotorFW, OUTPUT);
-  pinMode(speedControlL, OUTPUT);
-  pinMode(speedControlR, OUTPUT);
-  pinMode(LED, OUTPUT);
-  Serial.begin(115200);
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//this is to initialize the ultrasonic sensors which we are phasing out
-/*
-int pulseUltra(int echo, int trig) {
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echo, HIGH);
-  // Calculating the distance in cm
-  int distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  
-  return distance;
-}
-*/
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//movement functions to move the car
+/*MOVEMENT BLOCK*/
+/*DESCRIPTION: This block defines the controls sent to the motor driver to set the RPM of the wheels of the sumo bot. Each function is
+               named to match exactly what the sumo bot should do.*/
 void forward(){
   analogWrite(speedControlL,110);
   analogWrite(speedControlR,115);
@@ -187,126 +143,52 @@ void stop() {
   digitalWrite(rightMotorBW, 0);
   digitalWrite(rightMotorFW, 0);
 }
+/*END OF MOVEMENT BLOCK*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//This portion was used to trigger the ultrasonic sensors
-/*
-void trigAll() {
-    distanceM = pulseUltra(echoPin1, trigPin1);
-    distanceL = pulseUltra(echoPin2, trigPin2);
-    distanceR = pulseUltra(echoPin3, trigPin3);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*SETUP BLOCK*/
+/*DESCRIPTION: This block is used to set the modes of each pin that we initialize in the INITIALIZE BLOCK above*/
+void setup() {
+  //Below we set the input of the E18-D80NK IR sensors
+  pinMode(frontLeftSensor, INPUT_PULLUP);
+  pinMode(frontMiddleSensor, INPUT_PULLUP);
+  pinMode(frontRightSensor, INPUT_PULLUP);
+  //Below we set the input of the Arduino IR sensors 
+  pinMode(IRFrontCenter, INPUT);
+  pinMode(IRBackCenter, INPUT);
+  pinMode(IRFrontLeft, INPUT);
+  pinMode(IRFrontRight, INPUT);
+  //Below we set the input of the TT motors through the L298N Motor driver
+  pinMode(leftMotorFW, OUTPUT);
+  pinMode(leftMotorBW, OUTPUT);
+  pinMode(rightMotorBW, OUTPUT);
+  pinMode(rightMotorFW, OUTPUT);
+  pinMode(speedControlL, OUTPUT);
+  pinMode(speedControlR, OUTPUT);
+  
+  Serial.begin(115200);
+  /*END OF SETUP BLOCK*/
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-*/
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void blinkLED(){
-  digitalWrite(25, 1);
-  delay(1000);
-  digitalWrite(25, 0);
-  delay(1000);
-  digitalWrite(25, 1);
-  delay(1000);
-  digitalWrite(25, 0);
-  delay(1000);
-  digitalWrite(25, 1);
-  delay(1000);
-}
- 
-int tooClose = 20;
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*LOOP BLOCK*/
+/*DESCRIPTION: This block is the main block where we run our main algorithm. In this block specifically, we will be running the 
+               hallway traversal code. */
 void loop() {
-  //robot car is delaying for 5 seconds with builtin LED blinking for 5s
-  //blinkLED();
-  //Do not need to trigger all ultrasonic sensors
-  //trigAll();
-  //////////////////////////////////////////////
-
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-  //Read all the front sensors
+  //Reading from all the E18-D80NK IR sensors for object detection
   int frontLeft = digitalRead(frontLeftSensor);
   int frontMiddle = digitalRead(frontMiddleSensor);
   int frontRight = digitalRead(frontRightSensor);
-  ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //Getting the boolean Values from the IR sensors for boundary detection.
+  //Reading from all the Arduino IR sensors for boundary detection.
   bool frontLeftIR = digitalRead(IRFrontLeft);
   bool frontMiddleIR = digitalRead(IRFrontCenter);
   bool frontRightIR = digitalRead(IRFrontRight);
   bool backIR = digitalRead(IRBackCenter);
 
-  /*
-  //Serial.println(distanceR);
-  bool mStatus, rStatus, lStatus;
-  if(distanceM < 25) mStatus = true;
-  else mStatus = false;
-  if(distanceR < 15) rStatus = true;
-  else rStatus = false;
-  if(distanceL < 15) lStatus = true;
-  else lStatus = false;
-  
-  if(!mStatus && !rStatus) {
-  forwardright();
-  //Serial.println("Forward right");
-  }
-  else if(mStatus && !rStatus) {
-    left();
-    delay(500);
-  }
-  else if((!mStatus && rStatus)  || (distanceR < 10))  {
-    forwardleft();
-    //Serial.println("Forward left");
-  }
-  
-  else if(mStatus && rStatus) {
-    left();
-    delay(600);
-  }
-  if(digitalRead(IRFrontCenter) && digitalRead(IRFrontLeft) && digitalRead(IRFrontRight)) {
-    left();
-    delay(1500);
-    while(true) {
-      trigAll();
-      
-      if(distanceM < 20) {
-        left();
-        delay(500);
-      }
-      else if(distanceL < 20) {
-        forwardl();
-      }
-      else if(distanceL < 20) {
-        forwardright();
-      }
-      else forward();
-    }
-    if(distanceM < 25) mStatus = true;
-    else mStatus = false;
-    if(distanceR < 15) rStatus = true;
-    else rStatus = false;
-    if(distanceL < 15) lStatus = true;
-    else lStatus = false;
-    
-    if(!mStatus && !lStatus) {
-    forwardleft();
-    }
-    else if(mStatus && !lStatus) {
-      right();
-      delay(400);
-    }
-    else if((!mStatus && lStatus)  || (distanceL < 10))  {
-      fasterright();
-    }
-    
-    else if(mStatus && lStatus) {
-      right();
-      delay(400);
-    }
-  }
-  }
-  */
-
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //This is the algorithm for the Hallway traversal. "Hug the wall" Strat
+  //Main algorithm for the Hallway traversal. "Hug the wall" Strat from the middle of the hallway. 
   //For the boundary detection subsystem, 0 = white and 1 = black
   //For the object detection subsystem, 0 = object detected and 1 = clear
   if(frontLeftIR == 0 && frontMiddleIR == 0 && frontRightIR == 0 && backIR == 0){
@@ -323,7 +205,7 @@ void loop() {
       forwardleft();
     }
     else if(frontLeftSensor == 0 && frontMiddleSensor == 1 && frontRightSensor == 1){
-      right();
+      forwardright();
     }
     else if(frontLeftSensor == 0 && frontMiddleSensor == 1 && frontRightSensor == 0){
       forward();
@@ -340,6 +222,7 @@ void loop() {
     left();
     delay(500);
   }
+  /*END OF LOOP BLOCK*/
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
